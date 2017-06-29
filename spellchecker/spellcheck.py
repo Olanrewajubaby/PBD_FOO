43be0b2e9ee1a1497acbe19337501c4f9d4c3c24
Your Name <you@example.com>
2017-06-29 12:11:54 +0100

    Handle multiple failed words.

--- spellchecker/spellcheck.py
+++ spellchecker/spellcheck.py
@@ -1,22 +1,25 @@
 
 class SpellChecker:(object)
-    def __init__(self):
+def __init__(self):
         self.words = []
 		
-    def load_words(self, file_name):
+def load_words(self, file_name):
         self.words = open(file_name).readlines()
         self.words = map(lambda x: x.strip(), self.words)
 		
-    def check_word(self, word):
+def check_word(self, word):
         return word.strip('.').lower() in self.words
 		
-    def check_words(self, sentence):
+def check_words(self, sentence):
         words_to_check = sentence.split(' ')
+        failed_words = []
         for word in words_to_check:
             if not self.check_word(word):
                  print('Word is misspelt : ' + word)
-                 return False
-        return True	
+                 failed_words.append(word)
+                 return failed_words
+				 
+        
 		
 # spell_check = SpellChecker()
 # words = spell_check.load_words("spell.words")	
@@ -26,6 +29,6 @@ class SpellChecker:(object)
 
 if __name__ == '__main__':
     spellChecker = SpellChecker()
-    spellChecker.load_words('spell.words')
+ #   spellChecker.load_words('spell.words')
     print(spellChecker.check_word('zygotic'))
     print(spellChecker.check_words('zygotic mistasdas elementary'))
